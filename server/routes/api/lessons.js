e763b63d19e63f987a2038bfe54011afc6006e0e
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2021-01-05 19:18:52 +0200

    get request in postman working

--- server/routes/api/lessons.js
+++ server/routes/api/lessons.js
@@ -3,18 +3,28 @@ const mongodb = require("mongodb");
 
 const router = express.Router();
 
-// Get
+// Get Lessons
 router.get('/', async (req, res) => {
     const lessons = await loadLessonsCollection();
     res.send(await lessons.find({}).toArray());
 });
 
+// Add Lessons
+router.post('/', async (req, res) => {
+    const lessons = await loadLessonsCollection();
+    await lessons.insertOne({})
+});
+
+// Delete Lessons
+
+
+// Loading collection and connecting to a MongoDB
 async function loadLessonsCollection() {
     const client = await mongodb.MongoClient.connect('mongodb+srv://dbAdmin:admin123@mobileappcw2.ovizx.mongodb.net/<dbname>?retryWrites=true&w=majority', {
         useUnifiedTopology: true
     }); 
 
-    return client.db('MobileAppCW2').collection('Lessons');
+    return client.db('Coursework2').collection('Lessons');
 };
 
 
