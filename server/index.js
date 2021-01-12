a48c5881ac3fc8842140e0ff9e417c4cae9317a5
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2021-01-12 18:52:17 +0200

    added static folder and prepared SPA

--- server/index.js
+++ server/index.js
@@ -23,6 +23,15 @@ app.use('/api/lessons', lessons);
 app.use('/api/orders', orders);
 app.use('/api/orderupdate', orderupdate); 
 
+// hande production
+if(process.env.NODE_ENV == 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/dist/'));
+
+    // Handle SPA
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/dist/index.html')); // refer to any route and look into index.html
+}
+
 const port = process.env.PORT || 5000; // PORT for Heroku
 
 app.listen(port, () => console.log('Server started on port ${port}'));
